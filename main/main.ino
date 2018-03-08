b795fb83f6d94aadfb2ff69452b55eaafc880f3d
Eskild Trøen <eskildht@stud.ntnu.no>
2018-03-08 17:49:32 +0100

    Boarder detect eks

--- main/main.ino
+++ main/main.ino
@@ -1,36 +1,64 @@
-/*
-Demonstrates one second simple drive forward in setup
-*/
-// ESKILD!
-// Jan!
-// Kristoffer!
-// rahim
-//dette er noe
+//*Drive forward and turn left or right when border is detected
+//- Only reflectionsensor 0 and 5 are used.
 
 #include <ZumoMotors.h>
 #include <Pushbutton.h>
-
-
+#include <QTRSensors.h>
+#include <ZumoReflectanceSensorArray.h>
+ 
+#define LED 13
+ 
+// this might need to be tuned for different lighting conditions, surfaces, etc.
+#define QTR_THRESHOLD  1500 // 
+  
+// these might need to be tuned for different motor types
+#define REVERSE_SPEED     200 // 0 is stopped, 400 is full speed
+#define TURN_SPEED        200
+#define FORWARD_SPEED     400
+#define REVERSE_DURATION  200 // ms
+#define TURN_DURATION     300 // ms
+ 
 ZumoMotors motors;
-Pushbutton button(ZUMO_BUTTON);
+Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
+ 
+#define NUM_SENSORS 6
+unsigned int sensor_values[NUM_SENSORS];
+ 
+ZumoReflectanceSensorArray sensors;
 
 
-// This is the maximum speed the motors will be allowed to turn.
-// (400 lets the motors go at top speed; decrease to impose a speed limit)
-const int MAX_SPEED = 400;
-
-
+ 
 void setup()
 {
-  // Wait for the user button to be pressed and released
-  button.waitForButton();
-
-  motors.setSpeeds(100,100); //set left and right motor speed
-  delay(1000);
-  motors.setSpeeds(0,0);
+   sensors.init();
+   button.waitForButton();
 }
 
 void loop()
 {
+  sensors.read(sensor_values);
 
+  if (sensor_values[0] < QTR_THRESHOLD)
+  {
+    // if leftmost sensor detects line, reverse and turn to the right
+    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+    delay(REVERSE_DURATION);
+    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+    delay(TURN_DURATION);
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+  else if (sensor_values[5] < QTR_THRESHOLD)
+  {
+    // if rightmost sensor detects line, reverse and turn to the left
+    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+    delay(REVERSE_DURATION);
+    motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+    delay(TURN_DURATION);
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+  else
+  {
+    // otherwise, go straight
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
 }
