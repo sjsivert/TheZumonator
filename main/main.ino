a95f8b0b721416321805ce659f727238e626da6f
nikzy <sindrejohan1@gmail.com>
2018-03-15 17:02:11 +0100

    fixed linting

--- main/main.ino
+++ main/main.ino
@@ -4,12 +4,12 @@ Zumo goes forward in constant speed  until the sonar detects something within ma
 If within range: the LED is turned on, Zumo turns slighly away (left) from object detected
 while turning servo forth and back (0-180) in steps.
 If border (black tape) is found, it turns either side.
-Pushbuttom must be pressed to start Zumo.    
+Pushbuttom must be pressed to start Zumo.
     Sonar:  echoPin = 0
             triggerPin = 1
-    LED:    ledPin = 2 
+    LED:    ledPin = 2
     Servo:  servoPin = 6
-    
+
 */
 #include <NewPing.h>
 #include <NewServo.h>
@@ -22,8 +22,8 @@ Pushbuttom must be pressed to start Zumo.
 
 
 // this might need to be tuned for different lighting conditions, surfaces, etc.
-#define QTR_THRESHOLD  1900 // 
-  
+#define QTR_THRESHOLD  1900 //
+
 // these might need to be tuned for different motor types
 #define REVERSE_SPEED     200 // 0 is stopped, 400 is full speed
 #define TURN_SPEED        200
@@ -35,31 +35,31 @@ Pushbuttom must be pressed to start Zumo.
 
 PLab_ZumoMotors PLab_motors;
 
-const int echoPin = 0; 
-const int triggerPin = 1; 
+const int echoPin = 0;
+const int triggerPin = 1;
 // Max distance (in cm) of interest
-const int maxDistance = 37;  
+const int maxDistance = 37;
 const int ledPin = 2;
 const int servoPin = 6;
 // Servo start position degrees and Increments in servo movement
-int degreesServo = 90; 
-int degreesStep = 30;   
+int degreesServo = 90;
+int degreesStep = 30;
 
 #define NUM_SENSORS 6
 unsigned int sensor_values[NUM_SENSORS];
 // Initialize "sonar" with trigger pin, echo pin and max distance,
 // and then servo, motor, button and IR-sensor array
 NewPing sonar(triggerPin, echoPin, maxDistance);
-NewServo myServo;      
+NewServo myServo;
 ZumoMotors motors;
-Pushbutton button(ZUMO_BUTTON); 
+Pushbutton button(ZUMO_BUTTON);
 // Tell Zumo that we have no emitter-pin
-ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN); 
- 
- 
+ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
+
+
 void setup() {
    pinMode(ledPin,OUTPUT);
-   myServo.attach(servoPin); 
+   myServo.attach(servoPin);
    button.waitForButton();
    motors.setSpeeds(FORWARD_SPEED,FORWARD_SPEED);
 }
@@ -70,8 +70,8 @@ void loop() {
 }
 
 void keepLines(){
-       // Read IR-sensors and check if border detected
-   sensors.read(sensor_values);
+  // Read IR-sensors and check if border detected
+  sensors.read(sensor_values);
   if (sensor_values[0] < QTR_THRESHOLD)
   {
     // if leftmost sensor detects line, reverse and turn to the right
@@ -85,31 +85,30 @@ void keepLines(){
 }
 
 void servoSweep(){
-   for(int i=0; i<120; i++){
-  myServo.write(i);
-  unsigned int time = sonar.ping();
-  float distance = sonar.convert_cm(time);
-  if (! distance > 0) {
-   // No object in front
-   digitalWrite(ledPin,LOW); 
-   motors.setSpeeds(FORWARD_SPEED,FORWARD_SPEED);
- } else {
-   // Object detected
-   digitalWrite(ledPin,HIGH);
-  if(i<60){
-    PLab_motors.turnRight(FORWARD_SPEED, i);    
-  }
-  else{
-    PLab_motors.turnLeft(FORWARD_SPEED, i);  
-  }
-  motors.setSpeeds(400,400);
-  delay(800);
- }
-  }
+  for(int i=0; i<120; i++){
+      myServo.write(i);
+      unsigned int time = sonar.ping();
+      float distance = sonar.convert_cm(time);
+      if (! distance > 0) {
+       // No object in front
+       digitalWrite(ledPin,LOW);
+       motors.setSpeeds(FORWARD_SPEED,FORWARD_SPEED);
+      } else {
+       // Object detected
+      digitalWrite(ledPin,HIGH);
+      if(i<60){
+          PLab_motors.turnRight(FORWARD_SPEED, i);
+      } else{
+          PLab_motors.turnLeft(FORWARD_SPEED, i);
+      }
+          motors.setSpeeds(400,400);
+          delay(800);
+      }
+    }
 }
-  
 
-void turn(int direction){ 
+
+void turn(int direction){
 //
 // Move Zumo backwards, then turn in given direction,
 // and continue forward
@@ -130,6 +129,5 @@ void turn(int direction){
     motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
     delay(TURN_DURATION);
     motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
-  }  
+  }
 }
-
